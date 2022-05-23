daecc204886094c0ac67366cc204827f7de537f0
GitHub Action <action@github.com>
2022-05-23 06:25:13 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/formzero/intellijplatformplugintest/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.formzero.intellijplatformplugintest.services
+
+import com.github.formzero.intellijplatformplugintest.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
